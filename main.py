58ca829726767d1b54f92cc47544167307d1f739
Sandeep Vura <>
2021-05-16 13:30:45 +0000

    Adding string concatination.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
+#Print the strings in new line
+print("Hello World!\nHello World!\nHello World!")
+
+##Concate the string and add a space between two strings.
+print("Hello" + "Sandeep") ##When you run this line it doesn't contain space between the string.
+
+print("Hello" + " " + "Sandeep") # Adding space between the strings. 
